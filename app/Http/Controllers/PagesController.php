03ec360adb5944b775df4af503066d7b2d887e3e
melkotoury <melkotoury@gmail.com>
2018-04-20 19:58:54 +0200

    feat: collab data

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -53,10 +53,21 @@ class PagesController extends Controller
     public function recommended_recipe(){
         $id = Auth::user()->id;
         $currentuser = User::find($id);
+
+
+        //recipes current user likes
         $user_like_recipe_ids = Like::where('user_id',$id)->where('like', 1)->pluck('recipe_id');
 
+        //current user allergen and medical conditions
         $user_allergens = UserAllergen::where('user_id',$id)->pluck('name');
         $user_medical_conditions = UserMedicalCondition::where('user_id',$id)->pluck('name');
+
+        //get other users who loves the same recipes current user loves
+        foreach ($user_like_recipe_ids as $user_like_recipe_id){
+            $other_users_loves_my_recipes = Like::where('recipe_id',$user_like_recipe_id)->pluck('user_id');
+            $based_on_other_user_recipe_ids = Like::where('user_id',$other_users_loves_my_recipes)->pluck('recipe_id')->toArray();
+
+        }
 
         foreach ($user_like_recipe_ids as $user_like_recipe_id){
             $recipe_likes_allergens = RecipeAllergen::where('recipe_id',$user_like_recipe_id)->pluck('name');
@@ -78,10 +89,12 @@ class PagesController extends Controller
 
         $recipe_id = array_unique(array_merge($common_allergen_recipes,$common_medical_condition_recipes) , SORT_REGULAR);
         $recipe_likes_id = array_unique(array_merge($common_recipe_allergens_based_on_likes,$common_recipe_medical_conditions_based_on_likes),SORT_REGULAR);
+        $recipe_other_users_liked_id = array_unique($based_on_other_user_recipe_ids,SORT_REGULAR);
         return view('pages.recommended_recipe',
                         [
                             'recipe_id' => $recipe_id,
-                            'recipe_likes_id' => $recipe_likes_id
+                            'recipe_likes_id' => $recipe_likes_id,
+                            'recipe_other_users_liked_id' => $recipe_other_users_liked_id
 
                             ]);
     }
